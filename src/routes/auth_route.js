4a00999833c7f841078b7ebdb24134d8cf3e9f04
Bildan Jauhary <bildanjauhary@gmail.com>
2026-08-03 17:05:35 +0700

    feat: start working with swagger implement in register and login endpoints

--- src/routes/auth_route.js
+++ src/routes/auth_route.js
@@ -2,7 +2,62 @@ import { Router } from "express";
 import * as authControllers from "../controllers/auth.ctrl.js"
 
 const authRoutes = Router()
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *    description: Create Account Through Register
+ *    tags:
+ *     - Auth
+ *    requestBody:
+ *      description: Create new account
+ *      content:
+ *        application/x-www-form-urlencoded:
+ *           schema:
+ *            type: object
+ *            properties:
+ *               name:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                  type: string
+ *    responses:
+ *      "200":
+ *         description: Success create account
+ *      "404":
+ *         description: Email already being used
+ *    
+*/
 authRoutes.post("/register", authControllers.Register)
+
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *    description: Login
+ *    tags:
+ *     - Auth
+ *    requestBody:
+ *      description: Login into system
+ *      content:
+ *        application/x-www-form-urlencoded:
+ *           schema:
+ *            type: object
+ *            properties:
+ *               email:
+ *                 type: string
+ *               password:
+ *                  type: string
+ *    responses:
+ *      "200":
+ *        description: Success Login
+ *      "401":
+ *        description: Unauthorized
+ *    
+*/
 authRoutes.post("/login", authControllers.Login)
 
 export default authRoutes
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,33 @@
 import { Router } from "express"
 import authRoutes from "./auth_route.js"
+import swaggerUi from "swagger-ui-express"
+import swaggerJSDoc from "swagger-jsdoc"
+
+const swaggerOpt = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "BeliMudah Backend",
+      version: "1.0.0",
+      description: "API Documetation for BeliMudah web app",
+    },
+    component: {
+      securitySchemes: {
+        token: {
+          type: "apiKey",
+          name: "Authorization",
+          in: "header"
+        }
+      }
+    }
+  },
+  apis: ["./src/routes/*route.js"],
+}
+
+const swagger = swaggerJSDoc(swaggerOpt)
 
 const routes = Router()
 routes.use("/auth", authRoutes)
+routes.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swagger))
 
 export default routes
